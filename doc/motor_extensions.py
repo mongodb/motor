3a484191123298607196b9c1eb4b85339e21dace
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-01-16 09:27:41 -0500

    Remove all of PyMongo's version annotations from Motor's docs

--- doc/motor_extensions.py
+++ doc/motor_extensions.py
@@ -15,12 +15,11 @@
 """Motor specific extensions to Sphinx."""
 
 import inspect
-from distutils.version import LooseVersion
 
-from docutils.nodes import (
-    field, list_item, paragraph, title_reference, field_list, field_body,
-    bullet_list, Text, field_name)
+from docutils.nodes import field, list_item, paragraph, title_reference
+from docutils.nodes import field_list, field_body, bullet_list, Text, field_name
 from sphinx.addnodes import desc, desc_content, versionmodified, desc_signature
+from sphinx.addnodes import desc_name
 from sphinx.util.inspect import getargspec, safe_getattr
 from sphinx.ext.autodoc import MethodDocumenter, AttributeDocumenter
 
@@ -39,15 +38,16 @@ class MotorAttribute(object):
         self.name = name
         self.delegate_property = delegate_property
         self.pymongo_attr = getattr(motor_class.__delegate_class__, name)
-        if self.is_async_method():
-            full_name = '%s.%s.%s' % (
-                self.motor_class.__module__, self.motor_class.__name__,
-                self.name)
 
-            motor_info[full_name] = {
-                'is_async_method': self.is_async_method(),
-                'requires_callback': self.requires_callback(),
-            }
+        # Store some info for process_motor_nodes()
+        full_name = '%s.%s.%s' % (
+            self.motor_class.__module__, self.motor_class.__name__,
+            self.name)
+
+        motor_info[full_name] = {
+            'is_async_method': self.is_async_method(),
+            'requires_callback': self.requires_callback(),
+        }
 
     def is_async_method(self):
         return isinstance(self.delegate_property, motor.Async)
@@ -189,45 +189,47 @@ def insert_callback(parameters_node, callback_required):
 
 
 def process_motor_nodes(app, doctree):
+    # Search doctree for Motor's methods and attributes whose docstrings were
+    # copied from PyMongo, and fix them up for Motor:
+    #   1. Add a 'callback' param (sometimes optional, sometimes required) to
+    #      all async methods. If the PyMongo method took no params, we create
+    #      a parameter-list from scratch, otherwise we edit PyMongo's list.
+    #   2. Remove all version annotations like "New in version 2.0" since
+    #      PyMongo's version numbers are meaningless in Motor's docs.
     for objnode in doctree.traverse(desc):
-        if objnode['objtype'] == 'method':
-            name = find_by_path(objnode, [desc_signature])[0]['names'][0]
-            method_motor_info = motor_info.get(name)
-            if not (
-                method_motor_info and method_motor_info.get('is_async_method')
-            ):
-                # Not a special Motor method
-                continue
+        if objnode['objtype'] in ('method', 'attribute'):
+            signature_node = find_by_path(objnode, [desc_signature])[0]
+            name = '.'.join([
+                signature_node['module'], signature_node['fullname']])
 
-            desc_content_node = find_by_path(objnode, [desc_content])[0]
+            assert name.startswith('motor.')
+            obj_motor_info = motor_info.get(name)
+            if obj_motor_info:
+                desc_content_node = find_by_path(objnode, [desc_content])[0]
+                if obj_motor_info.get('is_async_method'):
+                    try:
+                        # Find the parameter list, a bullet_list instance
+                        parameters_node = find_by_path(desc_content_node,
+                            [field_list, field, field_body, bullet_list])[0]
+                    except IndexError:
+                        # PyMongo method has no parameters, create an empty
+                        # params list
+                        parameters_node = bullet_list()
+                        parameters_field_list_node = field_list('',
+                            field('',
+                                field_name('', 'Parameters '),
+                                field_body('',
+                                    parameters_node)))
 
-            try:
-                # Find the parameter list, a bullet_list instance
-                parameters_node = find_by_path(desc_content_node,
-                    [field_list, field, field_body, bullet_list])[0]
-            except IndexError:
-                # PyMongo method has no parameters, create an empty params list
-                parameters_node = bullet_list()
-                parameters_field_list_node = field_list('',
-                    field('',
-                        field_name('', 'Parameters '),
-                        field_body('',
-                            parameters_node)))
+                        desc_content_node.append(parameters_field_list_node)
 
-                desc_content_node.append(parameters_field_list_node)
+                    insert_callback(
+                        parameters_node, obj_motor_info['requires_callback'])
 
-            insert_callback(
-                parameters_node, method_motor_info['requires_callback'])
-
-            # Remove "versionadded", "versionchanged" and "deprecated"
-            # directives, if they refer to PyMongo changes prior to Motor.
-            # All are of type "versionmodified".
-            version_nodes = find_by_path(desc_content_node, [versionmodified])
-            for version_node in version_nodes:
-                version = LooseVersion(version_node['version'])
-                # TODO: set this to the first PyMongo release with Motor!
-                min_version = LooseVersion('2.6')
-                if version < min_version:
+                # Remove all "versionadded", "versionchanged" and "deprecated"
+                # directives from the docs we imported from PyMongo
+                version_nodes = find_by_path(desc_content_node, [versionmodified])
+                for version_node in version_nodes:
                     version_node.parent.remove(version_node)
 
 
