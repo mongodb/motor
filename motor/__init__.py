21cfe5ffffe1d5d5bd89d4ca7d04e1919f2f77ce
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-01-22 16:16:02 -0500

    Move yield points' docs from RST into docstrings in motor/__init__.py

--- motor/__init__.py
+++ motor/__init__.py
@@ -1741,8 +1741,32 @@ class MotorGridFS(MotorOpenable):
             return MotorGridOut(obj, io_loop=self.get_io_loop())
 
 
-# TODO: move docstrings from generator_interface.rst to here, include there
 class Op(gen.Task):
+    """Subclass of `tornado.gen.Task`_. Runs a single asynchronous MongoDB
+    operation and resumes the generator with the result of the operation when
+    it is complete. :class:`motor.Op` adds an additional convenience to
+    ``Task``: it assumes the callback is passed the standard ``result, error``
+    pair, and if ``error`` is not ``None`` then ``Op`` raises the error.
+    Otherwise, ``result`` is returned from the ``yield`` expression::
+
+        from tornado import gen
+
+        @gen.engine
+        def get_some_documents(db):
+            cursor = db.collection.find().limit(10)
+
+            try:
+                # to_list is passed a callback, which is later executed with
+                # arguments (result, error). If error is not None, it is raised
+                # from this line. Otherwise 'result' is the value of the yield
+                # expression.
+                documents = yield motor.Op(cursor.to_list)
+                return documents
+            except Exception, e:
+                print e
+
+    .. _tornado.gen.Task: http://www.tornadoweb.org/documentation/gen.html#tornado.gen.Task
+    """
     def __init__(self, func, *args, **kwargs):
         super(Op, self).__init__(func, *args, **kwargs)
 
@@ -1754,6 +1778,21 @@ class Op(gen.Task):
 
 
 class WaitOp(gen.Wait):
+    """For complex control flows, a :class:`motor.Op` can be split into two
+    parts, a `Callback`_ and a :class:`motor.WaitOp`::
+
+        @gen.engine
+        def get_some_documents(db):
+            cursor = db.collection.find().limit(10)
+            cursor.to_list(callback=(yield gen.Callback('key')))
+            try:
+                documents = yield motor.WaitOp('key')
+                return documents
+            except Exception, e:
+                print e
+
+    .. _Callback: http://www.tornadoweb.org/documentation/gen.html#tornado.gen.Callback
+    """
     def get_result(self):
         (result, error), _ = super(WaitOp, self).get_result()
         if error:
@@ -1763,6 +1802,23 @@ class WaitOp(gen.Wait):
 
 
 class WaitAllOps(gen.WaitAll):
+    """To wait for multiple Callbacks to complete, yield
+    :class:`motor.WaitAllOps`::
+
+        @gen.engine
+        def get_two_documents_in_parallel(db, id_one, id_two):
+            db.collection.find_one(
+                {'_id': id_one}, callback=(yield gen.Callback('one')))
+
+            db.collection.find_one(
+                {'_id': id_two}, callback=(yield gen.Callback('two')))
+
+            try:
+                document_one, document_two = yield motor.WaitAllOps(['one', 'two'])
+                return document_one, document_two
+            except Exception, e:
+                print e
+    """
     def get_result(self):
         super_results = super(WaitAllOps, self).get_result()
 
