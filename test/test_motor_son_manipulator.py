a23361b4b8330eaef4fb61d93e0db1bd168e1b28
Eren Güven <erenguven0@gmail.com>
2013-04-28 09:41:00 +0300

    remove motor.Op from son_manipulator tests

--- test/test_motor_son_manipulator.py
+++ test/test_motor_son_manipulator.py
@@ -34,7 +34,7 @@ class SONManipulatorTest(MotorTest):
     @gen_test
     def test_with_find_one(self):
         coll = self.coll
-        _id = yield motor.Op(coll.insert, {'foo': 'bar'})
+        _id = yield coll.insert({'foo': 'bar'})
         yield AssertEqual(
             {'_id': _id, 'foo': 'bar'},
             coll.find_one)
@@ -48,7 +48,7 @@ class SONManipulatorTest(MotorTest):
     def test_with_fetch_next(self):
         coll = self.coll
         coll.database.add_son_manipulator(CustomSONManipulator())
-        _id = yield motor.Op(coll.insert, {'foo': 'bar'})
+        _id = yield coll.insert({'foo': 'bar'})
         cursor = coll.find()
         self.assertTrue((yield cursor.fetch_next))
         self.assertEqual(
@@ -58,14 +58,11 @@ class SONManipulatorTest(MotorTest):
     @gen_test
     def test_with_to_list(self):
         coll = self.coll
-        _id1, _id2 = yield motor.Op(coll.insert, [{}, {}])
-        yield AssertEqual(
-            [{'_id': _id1}, {'_id': _id2}],
-            coll.find().sort([('_id', pymongo.ASCENDING)]).limit(2).to_list)
+        _id1, _id2 = yield coll.insert([{}, {}])
+        found = yield coll.find().sort([('_id', pymongo.ASCENDING)]).to_list(length=2)
+        self.assertEqual([{'_id': _id1}, {'_id': _id2}], found)
+
         coll.database.add_son_manipulator(CustomSONManipulator())
-        yield AssertEqual(
-            [{'_id': _id1, 'added_field': 42}, {'_id': _id2, 'added_field': 42}],
-            coll.find().sort([('_id', pymongo.ASCENDING)]).limit(2).to_list)
-        yield AssertEqual(
-            [{'_id': _id1, 'added_field': 42}],
-            coll.find().sort([('_id', pymongo.ASCENDING)]).to_list, length=1)
+        expected = [{'_id': _id1, 'added_field': 42}, {'_id': _id2, 'added_field': 42}]
+        found = yield coll.find().sort([('_id', pymongo.ASCENDING)]).to_list(length=2)
+        self.assertEqual(expected, found)
