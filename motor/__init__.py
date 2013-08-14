c598ad9d337e87ecfb8623d9acfb8fe8d944b60f
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-08-14 12:08:51 -0400

    PyMongo 2.6 compatibility: override Monitor.start_sync.

--- motor/__init__.py
+++ motor/__init__.py
@@ -1322,6 +1322,11 @@ class MotorReplicaSetMonitor(pymongo.mongo_replica_set_client.Monitor):
            the monitor separately to ensure it uses the right IOLoop"""
         pass
 
+    def start_sync(self):
+        """No-op: PyMongo thinks this starts the monitor, but Motor starts
+           the monitor separately to ensure it uses the right IOLoop"""
+        pass
+
     def start_motor(self, io_loop):
         self.io_loop = io_loop
         self.started = True
