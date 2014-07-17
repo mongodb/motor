16fd329057b93c67a4b7d1731563d5f8dc30fc47
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-07-17 18:13:25 -0400

    MOTOR-40 Update tests' docstrings.

--- test/asyncio_tests/test_asyncio_collection.py
+++ test/asyncio_tests/test_asyncio_collection.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test MotorCollection with asyncio."""
+"""Test AsyncIOMotorCollection."""
 
 import asyncio
 import unittest
