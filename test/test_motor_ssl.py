f1755ca75f5c72d1bc3d18a015c184120c655d5d
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-11-26 12:06:51 -0500

    Test certificate-error message with Tornado.

--- test/test_motor_ssl.py
+++ test/test_motor_ssl.py
@@ -270,8 +270,9 @@ class MotorSSLTest(MotorTest):
 
             yield client.db.collection.find_one()
             self.fail("Invalid hostname should have failed")
-        except ConnectionFailure:
-            pass
+        except ConnectionFailure as exc:
+            self.assertEqual("hostname 'localhost' doesn't match 'server'",
+                             str(exc))
 
         if 'setName' in response:
             try:
