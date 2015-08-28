fa6de8c90275362dc211eba7772420a07d459b41
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-08-28 07:19:44 -0400

    MOTOR-76 Don't require Tornado.
    Move Tornado-specific code to motor_tornado.py. Conditionally import
    in motor/__init__ for backwards compatibility.

---
+++ motor/__init__.py
@@ -0,0 +1,58 @@
+# Copyright 2011-2015 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Motor, an asynchronous driver for MongoDB."""
+
+from __future__ import unicode_literals, absolute_import
+
+import pymongo
+
+version_tuple = (0, 5, 'dev0')
+
+
+def get_version_string():
+    return '.'.join(str(v) for v in version_tuple)
+
+version = get_version_string()
+"""Current version of Motor."""
+
+expected_pymongo_version = '2.8'
+if pymongo.version != expected_pymongo_version:
+    msg = (
+        "Motor %s requires PyMongo at exactly version %s. "
+        "You have PyMongo %s. "
+        "Do pip install pymongo==2.8.0"
+    ) % (version, expected_pymongo_version, pymongo.version)
+
+    raise ImportError(msg)
+
+try:
+    import tornado
+except ImportError:
+    tornado = None
+else:
+    # For backwards compatibility with Motor 0.4, export Motor's Tornado classes
+    # at module root. This may change in Motor 1.0. First get __all__.
+    from .motor_tornado import *
+
+    # Now some classes that aren't in __all__ but might be expected.
+    from .motor_tornado import (MotorCollection,
+                                MotorDatabase,
+                                MotorGridFS,
+                                MotorGridIn,
+                                MotorGridOut,
+                                MotorBulkOperationBuilder)
+
+    # Make "from motor import *" the same as "from motor.motor_tornado import *"
+    from .motor_tornado import __all__
--- motor/motor_tornado.py
+++ motor/motor_tornado.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2014 MongoDB, Inc.
+# Copyright 2011-2015 MongoDB, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,37 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import unicode_literals, absolute_import
+"""Tornado support for Motor, an asynchronous driver for MongoDB."""
 
-"""Motor, an asynchronous driver for MongoDB."""
+from __future__ import unicode_literals, absolute_import
 
 import warnings
 
-import pymongo
-
-version_tuple = (0, 5, 'dev0')
-
-
-def get_version_string():
-    return '.'.join(str(v) for v in version_tuple)
-
-version = get_version_string()
-"""Current version of Motor."""
-
-expected_pymongo_version = '2.8'
-if pymongo.version != expected_pymongo_version:
-    msg = (
-        "Motor %s requires PyMongo at exactly version %s. "
-        "You have PyMongo %s. "
-        "Do pip install pymongo==2.8.0"
-    ) % (version, expected_pymongo_version, pymongo.version)
-
-    raise ImportError(msg)
-
-from . import core, motor_gridfs, motor_py3_compat, util
+from . import core, motor_gridfs
 from .frameworks import tornado as tornado_framework
 from .metaprogramming import create_class_with_framework
-from .motor_common import callback_type_error
 
 __all__ = ['MotorClient', 'MotorReplicaSetClient', 'Op']
 
