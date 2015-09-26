b2b375991ff6e5bd7e9262a76c296384da2fd505
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-09-25 21:24:36 -0400

    MOTOR-86 Support Python 3.5.
    Add Python 3.5 to docs, tox, trove specifiers.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Operating System :: MacOS :: MacOS X
 Operating System :: Unix
 Programming Language :: Python
