3684b49ced550daa4e2cf049104d8ce2a3da40f4
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-10-02 23:49:55 -0400

    Post-release bump.

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ if sys.version_info[0] >= 3:
     packages.append('motor.frameworks.asyncio')
 
 setup(name='motor',
-      version='0.7b0',
+      version='0.7.dev0',
       packages=packages,
       description=description,
       long_description=long_description,
