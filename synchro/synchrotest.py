b43273266711ba07c62a3ba2c4d3efb759ce47f8
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-10-31 15:41:55 -0400

    Skip unreliable synchrotest.

--- synchro/synchrotest.py
+++ synchro/synchrotest.py
@@ -127,6 +127,9 @@ excluded_tests = [
     'TestReplicaSetClientInternalIPs.*',
     'TestReplicaSetClientMaxWriteBatchSize.*',
 
+    # Motor is correct here, it's just unreliable on slow CI servers.
+    'TestReplicaSetClient.test_timeout_does_not_mark_member_down',
+
     # Accesses PyMongo internals.
     'TestClient.test_kill_cursor_explicit_primary',
     'TestClient.test_kill_cursor_explicit_secondary',
