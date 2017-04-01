3ce7594c57f24a4c684e7c92dff43ce3533e0880
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-04-01 06:52:22 -0400

    Update PyMongo intersphinx link

--- doc/conf.py
+++ doc/conf.py
@@ -183,7 +183,7 @@ latex_documents = [
 autodoc_default_flags = ['inherited-members']
 autodoc_member_order = 'groupwise'
 
-pymongo_inventory = ('http://api.mongodb.org/python/%s/' % pymongo_version,
+pymongo_inventory = ('http://api.mongodb.com/python/%s/' % pymongo_version,
                      None)
 
 intersphinx_mapping = {
