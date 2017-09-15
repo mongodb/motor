bb9c168f29f4887158dd48cc7f7b78e69280f723
Karol Horowski <karol.hor@gmail.com>
2017-09-15 10:18:41 +0200

    skip test_to_list_with_chained_collation if run with mongo < 3.4

--- test/tornado_tests/test_motor_cursor.py
+++ test/tornado_tests/test_motor_cursor.py
@@ -279,12 +279,15 @@ class MotorCursorTest(MotorMockServerTest):
 
     @gen_test
     def test_to_list_with_chained_collation(self):
+        if not (yield at_least(self.cx, (3, 4))):
+            raise SkipTest("collation requires MongoDB >= 3.4")
+
         yield self.make_test_data()
         cursor = self.collection.find({}, {'_id': 1}) \
             .sort([('_id', pymongo.ASCENDING)]) \
             .collation(Collation("en"))
         expected = [{'_id': i} for i in range(200)]
-        (result, error), _ = yield gen.Task(cursor.to_list, length=1000)
+        result = yield cursor.to_list(length=1000)
         self.assertEqual(expected, result)
 
     @gen_test
