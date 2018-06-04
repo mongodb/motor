3591cb4b8a9881f4b22e50e51cf5e2bc174c790e
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-06-03 23:08:03 -0400

    MOTOR-225 Fix docs while awaiting PyMongo 3.7

--- doc/conf.py
+++ doc/conf.py
@@ -202,7 +202,10 @@ latex_documents = [
 autodoc_default_flags = ['inherited-members']
 autodoc_member_order = 'groupwise'
 
-pymongo_inventory = ('http://api.mongodb.com/python/%s/' % pymongo_version,
+sys.stderr.write('Warning: update PyMongo inventory to 3.7\n')
+# pymongo_inventory = ('http://api.mongodb.com/python/%s/' % pymongo_version,
+#                      None)
+pymongo_inventory = ('http://api.mongodb.com/python/3.6.1',
                      None)
 
 intersphinx_mapping = {
