da422872fdf396d1a36c26d2750c338fbd77e21f
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-07-11 03:26:18 -0400

    Version -> dev0

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ if sys.version_info[0] >= 3:
     packages.append('motor.aiohttp')
 
 setup(name='motor',
-      version='1.3.0',
+      version='1.3.1.dev0',
       packages=packages,
       description=description,
       long_description=long_description,
