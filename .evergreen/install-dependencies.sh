c26053581071d2c0e9239daddc19b44d151923f7
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-09-30 09:30:38 -0400

    Use drivers-evergreen-tools certs in tests

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -2,12 +2,6 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Copy Motor's test certificates over driver-evergreen-tools'
-cp ${PROJECT_DIRECTORY}/test/certificates/* ${DRIVERS_TOOLS}/.evergreen/x509gen/
-
-# Replace MongoOrchestration's client certificate.
-cp ${PROJECT_DIRECTORY}/test/certificates/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
-
 if [ -w /etc/hosts ]; then
   SUDO=""
 else
