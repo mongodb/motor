d3c1f284be57c34c10801f56b090b77982c46631
Prashant Mital <prashantmital@gmail.com>
2020-09-24 16:28:47 -0700

    BUMP 2.4.0.dev0

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ packages = ['motor', 'motor.frameworks', 'motor.frameworks.tornado',
 
 
 setup(name='motor',
-      version='2.3.0',
+      version='2.4.0.dev0',
       packages=packages,
       description=description,
       long_description=long_description,
