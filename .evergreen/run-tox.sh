f2615ed9a61e3aa8612b687c282d6540756596f8
William Zhou <william.zhou@mongodb.com>
2021-03-29 17:56:23 -0700

    replace with pip install

--- .evergreen/run-tox.sh
+++ .evergreen/run-tox.sh
@@ -54,48 +54,7 @@ if [ -n "$TEST_ENCRYPTION" ]; then
     trap "deactivate; rm -rf venv-encryption" EXIT HUP
     PYTHON=python
 
-    if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
-        $PYTHON -m pip install -U setuptools
-    fi
-
-    if [ -z "$LIBMONGOCRYPT_URL" ]; then
-        echo "Cannot test client side encryption without LIBMONGOCRYPT_URL!"
-        exit 1
-    fi
-    curl -O "$LIBMONGOCRYPT_URL"
-    mkdir libmongocrypt
-    tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
-    ls -la libmongocrypt
-    ls -la libmongocrypt/nocrypto
-    # Use the nocrypto build to avoid dependency issues with older windows/python versions.
-    BASE=$(pwd)/libmongocrypt/nocrypto
-    if [ -f "${BASE}/lib/libmongocrypt.so" ]; then
-        export PYMONGOCRYPT_LIB=${BASE}/lib/libmongocrypt.so
-    elif [ -f "${BASE}/lib/libmongocrypt.dylib" ]; then
-        export PYMONGOCRYPT_LIB=${BASE}/lib/libmongocrypt.dylib
-    elif [ -f "${BASE}/bin/mongocrypt.dll" ]; then
-        PYMONGOCRYPT_LIB=${BASE}/bin/mongocrypt.dll
-        # libmongocrypt's windows dll is not marked executable.
-        chmod +x $PYMONGOCRYPT_LIB
-        export PYMONGOCRYPT_LIB=$(cygpath -m $PYMONGOCRYPT_LIB)
-    elif [ -f "${BASE}/lib64/libmongocrypt.so" ]; then
-        export PYMONGOCRYPT_LIB=${BASE}/lib64/libmongocrypt.so
-    else
-        echo "Cannot find libmongocrypt shared object file"
-        exit 1
-    fi
-
-    # TODO: Test with 'pip install pymongocrypt'
-    git clone --branch master https://github.com/mongodb/libmongocrypt.git libmongocrypt_git
-    python -m pip install --prefer-binary -r .evergreen/test-encryption-requirements.txt
-    python -m pip install ./libmongocrypt_git/bindings/python
-    python -c "import pymongocrypt; print('pymongocrypt version: '+pymongocrypt.__version__)"
-    python -c "import pymongocrypt; print('libmongocrypt version: '+pymongocrypt.libmongocrypt_version())"
-    # PATH is updated by PREPARE_SHELL for access to mongocryptd.
-
-    # Get access to the AWS temporary credentials:
-    # CSFLE_AWS_TEMP_ACCESS_KEY_ID, CSFLE_AWS_TEMP_SECRET_ACCESS_KEY, CSFLE_AWS_TEMP_SESSION_TOKEN
-    . $DRIVERS_TOOLS/.evergreen/csfle/set-temp-creds.sh
+    python -m pip install pymongo[encryption]
 fi
 
 # Run the tests, and store the results in Evergreen compatible XUnit XML
