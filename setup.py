07e1f056ee07805a3e657a1331e3b732ad402200
William Zhou <william.zhou@mongodb.com>
2021-04-04 16:56:09 -0700

    add 3.9 support to pypi

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Operating System :: MacOS :: MacOS X
 Operating System :: Unix
 Operating System :: Microsoft :: Windows
