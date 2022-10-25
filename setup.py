4d2713c7b19fac70928a77cd689d56871c7071b8
Steven Silvester <steven.silvester@ieee.org>
2022-10-25 15:49:26 -0500

    BUMP 3.1.1

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ packages = [
 
 setup(
     name="motor",
-    version="3.1.0.dev0",
+    version="3.1.1",
     packages=packages,
     description=description,
     long_description=long_description,
