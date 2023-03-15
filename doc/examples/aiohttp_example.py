f77fa40ec57286315b258a362931c1e514a08b9a
Shashank Nigam <shashanknigam40@gmail.com>
2023-03-15 23:30:58 +0800

    Update aiohttp example (#197)
    Co-authored-by: Steven Silvester <steven.silvester@ieee.org>

--- doc/examples/aiohttp_example.py
+++ doc/examples/aiohttp_example.py
@@ -1,7 +1,5 @@
 # These comments let tutorial-asyncio.rst include this code in sections.
 # -- setup-start --
-import asyncio
-
 from aiohttp import web
 
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -41,10 +39,20 @@ async def page_handler(request):
 # -- handler-end --
 
 # -- main-start --
-db = asyncio.run(setup_db())
-app = web.Application()
-app["db"] = db
-# Route requests to the page_handler() coroutine.
-app.router.add_get("/pages/{page_name}", page_handler)
-web.run_app(app)
+async def init_connection():
+    db = await setup_db()
+    app = web.Application()
+    app["db"] = db
+    # Route requests to the page_handler() coroutine.
+    app.router.add_get("/pages/{page_name}", page_handler)
+    return app
+
+
+def main():
+    app = init_connection()
+    web.run_app(app)
+
+
+if __name__ == "__main__":
+    main()
 # -- main-end --
