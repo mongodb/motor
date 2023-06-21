038ab523b25f9a7a341a05c2789393a098ca2b15
Steven Silvester <steven.silvester@ieee.org>
2023-06-21 14:48:45 -0500

    MOTOR-1143 [Failure] test_async_create_encrypted_collection

--- test/asyncio_tests/test_asyncio_collection.py
+++ test/asyncio_tests/test_asyncio_collection.py
@@ -266,7 +266,7 @@ class TestAsyncIOCollection(AsyncIOTestCase):
             self.assertEqual(read_preference, c.read_preference)
             self.assertEqual(codec_options, c.codec_options)
 
-    @env.require_version_min(6, 2, -1, -1)
+    @env.require_version_min(7, 0, -1, -1)
     @env.require_no_standalone
     @asyncio_test
     async def test_async_create_encrypted_collection(self):
