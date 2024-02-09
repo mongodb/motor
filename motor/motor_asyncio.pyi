6c7f4b5fbd77031bedbc29a54f55c6ac7bf66ec6
Noah Stapp <noah.stapp@mongodb.com>
2024-02-09 13:25:46 -0800

    MOTOR-1247 - Typing issue with motor_asyncio types

---
+++ motor/motor_asyncio.pyi
@@ -0,0 +1,34 @@
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+from motor import core, motor_gridfs
+
+AsyncIOMotorClient: TypeAlias = core.AgnosticClient
+
+AsyncIOMotorClientSession: TypeAlias = core.AgnosticClientSession
+
+AsyncIOMotorDatabase: TypeAlias = core.AgnosticDatabase
+
+AsyncIOMotorCollection: TypeAlias = core.AgnosticCollection
+
+AsyncIOMotorCursor: TypeAlias = core.AgnosticCursor
+
+AsyncIOMotorCommandCursor: TypeAlias = core.AgnosticCommandCursor
+
+AsyncIOMotorLatentCommandCursor: TypeAlias = core.AgnosticLatentCommandCursor
+
+AsyncIOMotorChangeStream: TypeAlias = core.AgnosticChangeStream
+
+AsyncIOMotorGridFSBucket: TypeAlias = motor_gridfs.AgnosticGridFSBucket
+
+AsyncIOMotorGridIn: TypeAlias = motor_gridfs.AgnosticGridIn
+
+AsyncIOMotorGridOut: TypeAlias = motor_gridfs.AgnosticGridOut
+
+AsyncIOMotorGridOutCursor: TypeAlias = motor_gridfs.AgnosticGridOutCursor
+
+AsyncIOMotorClientEncryption: TypeAlias = core.AgnosticClientEncryption
