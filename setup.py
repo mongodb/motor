e93b9693d41904756e8d8ee17a42ebc26e986db0
Steven Silvester <steven.silvester@ieee.org>
2024-04-18 19:45:31 -0500

    MOTOR-1299 Create separate requirements files (#276)

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
 from setuptools import setup
 
-setup()
+
+def parse_reqs_file(fname):
+    with open(fname) as fid:  # noqa:PTH123
+        lines = [li.strip() for li in fid.readlines()]
+    return [li for li in lines if li and not li.startswith("#")]
+
+
+extras_require = dict(  # noqa:C408
+    aws=parse_reqs_file("requirements/aws.txt"),
+    encryption=parse_reqs_file("requirements/encryption.txt"),
+    gssapi=parse_reqs_file("requirements/gssapi.txt"),
+    ocsp=parse_reqs_file("requirements/ocsp.txt"),
+    snappy=parse_reqs_file("requirements/snappy.txt"),
+    srv=parse_reqs_file("requirements/srv.txt"),
+    test=parse_reqs_file("requirements/test.txt"),
+    zstd=parse_reqs_file("requirements/zstd.txt"),
+)
+
+setup(install_requires=parse_reqs_file("requirements.txt"), extras_require=extras_require)
