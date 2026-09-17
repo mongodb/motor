c39bb9afa1dad7e1c1055ed99238a69ac7378511
Iris <iris.ho@mongodb.com>
2026-09-17 13:49:39 -0700

    run linter

--- motor/core.pyi
+++ motor/core.pyi
@@ -35,13 +35,20 @@ from bson.raw_bson import RawBSONDocument
 from pymongo import IndexModel, ReadPreference, WriteConcern
 from pymongo.change_stream import ChangeStream
 from pymongo.client_options import ClientOptions
-from pymongo.client_session import ClientSession, SessionOptions, TransactionOptions  # type: ignore[attr-defined,unused-ignore]
+from pymongo.client_session import (  # type: ignore[attr-defined,unused-ignore]
+    ClientSession,
+    SessionOptions,
+    TransactionOptions,
+)
 from pymongo.collection import Collection, ReturnDocument  # noqa: F401
 from pymongo.command_cursor import CommandCursor, RawBatchCommandCursor
 from pymongo.cursor import Cursor, RawBatchCursor
 from pymongo.cursor_shared import _Hint, _Sort
 from pymongo.database import Database
-from pymongo.encryption import ClientEncryption, RewrapManyDataKeyResult  # type: ignore[attr-defined,unused-ignore]
+from pymongo.encryption import (  # type: ignore[attr-defined,unused-ignore]
+    ClientEncryption,
+    RewrapManyDataKeyResult,
+)
 from pymongo.encryption_options import RangeOpts
 from pymongo.operations import _IndexKeyHint, _IndexList
 from pymongo.read_concern import ReadConcern
